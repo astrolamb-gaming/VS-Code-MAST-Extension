2db9fb658cf6428f0b85f1e56e4f88f0e376c2eb
astrolamb-gaming <astrolamb.gaming@gmail.com>
2025-02-24 15:49:13 -0500

    Testing python stuff

--- server/src/mastGlobals.py
+++ server/src/mastGlobals.py
@@ -1,39 +1,92 @@
 
 import sys
 import traceback
-from inspect import signature
+import inspect
+import numbers
+
 
 sys.modules['script'] = sys.modules.get('__main__')
-sbs_utilsPath = sys.argv[1] # Very important
-sbsPath = sys.argv[2]
-print(sbs_utilsPath)
+# sbs_utilsPath = sys.argv[1] # Very important
+# sbsPath = sys.argv[2]
+# print(sbs_utilsPath)
+
+# for testing:
+sbs_utilsPath = "D:\\Cosmos Dev\\Cosmos-1-0-1\\data\missions\\__lib__\\artemis-sbs.sbs_utils.v1.0.2.01.sbslib"
+sbsPath = "C:\\Users\\matts\\.vscode\\extensions\\mast\\server\\src\\sbs.zip"
+callables = []
+modules = []
+
+def compare():
+	global callables
+	global modules
+	for c in callables:
+		if c in modules:
+			print("Has the same")
 
 def getGlobals(globals):
 	count = 0
 	for k in globals:
-			test = globals[k]
-			if callable(test):
+		try:
+			# print(globals[k])
+			# print(test)
+			if callable(globals[k]):
+				callables.append(k)
+				print("Callable")
 				count += 1
-				try:
-					print(k)
-					#print(signature(test))
-					#print(test.__doc__)
-				except:
-					pass
-				#m = test.__module__
-				#print(f"{k} from {m}")
+				continue
+				# try:
+				# 	print("Callable")
+				# 	print(k)
+					
+				# 	#print(signature(test))
+				# 	#print(test.__doc__)
+				# except:
+				# 	exc_type, exc_value, exc_tb = sys.exc_info()
+				# 	stack_trace = ''.join(traceback.format_exception(exc_type, exc_value, exc_tb))
+				# 	print(stack_trace)
+				# 	continue
+
+			if ("module" in str(globals[k]) or "class" in str(globals[k])):
+				modules.append(globals[k])
+				if str(globals[k]) == "script":
+					continue
+				print("Module " + k)
+				funcs = inspect.getmembers(globals[k],inspect.isfunction)
+				print(funcs)
+				continue
+			if str(globals[k]).isnumeric():
+				print("Number")
+				continue
+			else:
+				print("OTHERS")
+				print(k)
+				print(globals[k])
+
+			#m = test.__module__
+			#print(f"{k} from {m}")
+		except Exception as e:
+				exc_type, exc_value, exc_tb = sys.exc_info()
+				stack_trace = ''.join(traceback.format_exception(exc_type, exc_value, exc_tb))
+				print(stack_trace)
+				continue
 	print("Count: " + str(count))
 
+# try:
+sys.path.append(sbsPath)
+sys.path.append(sbs_utilsPath)
+print(sbs_utilsPath)
+loaded = False
 try:
-	sys.path.append(sbsPath)
-	sys.path.append(sbs_utilsPath)
-	print(sbs_utilsPath)
+	from sbs_utils.mast.mast_sbs_procedural import * # type: ignore
+	#from sbs_utils.mast.mast import Mast # type: ignore
+	getGlobals(Mast.globals)
+	loaded = True
+except:
+	exc_type, exc_value, exc_tb = sys.exc_info()
+	stack_trace = ''.join(traceback.format_exception(exc_type, exc_value, exc_tb))
+	print(stack_trace)
+if not loaded:
 	try:
-		from sbs_utils.mast.mast_sbs_procedural import * # type: ignore
-		#from sbs_utils.mast.mast import Mast # type: ignore
-		getGlobals(Mast.globals)
-	except:
-
 		# Works
 		from sbs_utils.mast.mast_node import MastNode, mast_node, IF_EXP_REGEX
 		from sbs_utils.mast.mast import Mast
@@ -43,7 +96,13 @@ try:
 		# # type: ignore
 		
 		getGlobals(MastGlobals.globals)
-except Exception as e:
-	exc_type, exc_value, exc_tb = sys.exc_info()
-	stack_trace = ''.join(traceback.format_exception(exc_type, exc_value, exc_tb))
-	print(stack_trace)
+	except:
+		exc_type, exc_value, exc_tb = sys.exc_info()
+		stack_trace = ''.join(traceback.format_exception(exc_type, exc_value, exc_tb))
+		print(stack_trace)
+
+compare()
+# except Exception as e:
+# 	exc_type, exc_value, exc_tb = sys.exc_info()
+# 	stack_trace = ''.join(traceback.format_exception(exc_type, exc_value, exc_tb))
+# 	print(stack_trace)
