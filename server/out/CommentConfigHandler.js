01788538cb336cc26e98ed3ac90e679b1fe54980
astrolamb-gaming <astrolamb.gaming@gmail.com>
2025-10-23 11:02:35 -0400

    Update gitignore and remove contents of `out` folders

--- server/out/CommentConfigHandler.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=CommentConfigHandler.js.map
--- server/out/mastVariables.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.parseVariables = parseVariables;
-function parseVariables(textDocument) {
-}
-//# sourceMappingURL=mastVariables.js.map
